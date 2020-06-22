438df468388931e3712d9fc7a0768ce9364fee17
JoongSeokD <ljseokd@gmail.com>
2020-06-22 17:15:23 +0900

    이벤트 도메인 구현

---
+++ src/main/java/me/ljseokd/studyrestapi/events/EventStatus.java
@@ -0,0 +1,5 @@
+package me.ljseokd.studyrestapi.events;
+
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGIN_ENROLLMENT;
+}
---
+++ src/test/java/me/ljseokd/studyrestapi/events/EventTest.java
@@ -0,0 +1,35 @@
+package me.ljseokd.studyrestapi.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    void builder() {
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    void javaBean() {
+        // Given
+        String name = "Event";
+        String description = "Spring";
+
+        // When
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+
+    }
+}
