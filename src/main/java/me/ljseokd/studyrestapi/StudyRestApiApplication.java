e42934aba22a6591a3aebaea1cb2f2498599b7a4
JoongSeokD <ljseokd@gmail.com>
2020-06-22 16:52:40 +0900

    Initial commit

---
+++ src/main/java/me/ljseokd/studyrestapi/StudyRestApiApplication.java
@@ -0,0 +1,13 @@
+package me.ljseokd.studyrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyRestApiApplication.class, args);
+    }
+
+}
