3694fec7584f1a17a5240a85ca2a683650f524ee
JoongSeokD <ljseokd@gmail.com>
2020-06-24 11:29:54 +0900

    매개변수를 이용한 테스트
    @ParameterizedTest(name ="{index} => name{0}"
    @CsvSource({"ljseokd"})
    And
    @MethodSource("testParams")
    private static Object[] testParams() {
        return new Object[]{
                new Object[]{"ljseokd"}
        };
    }

--- src/test/java/me/ljseokd/studyrestapi/events/EventTest.java
+++ src/test/java/me/ljseokd/studyrestapi/events/EventTest.java
@@ -1,6 +1,9 @@
 package me.ljseokd.studyrestapi.events;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,74 +35,70 @@ class EventTest {
         assertThat(event.getDescription()).isEqualTo(description);
     }
 
-    @Test
-    void testFreeTrue(){
+    @ParameterizedTest(name = "{index} => basePrice={0}, maxPrice={1}, isFree={2}")
+    @CsvSource({
+            "0, 0, true",
+            "100, 0, false",
+            "0, 1000, false"
+    })
+    public void various_tests_isFree_NonSafety(int basePrice, int maxPrice, boolean isFree){
+        System.out.println(basePrice);
         // Given
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
-
-        // When
+        //When
         event.update();
 
         // Then
-        assertThat(event.isFree()).isTrue();
-    }
-    @Test
-    void testFreeFalse(){
-        // Given
-        Event event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
-    }
-    @Test
-    void testFreeFalse2(){
-        // Given
-        Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
+        assertThat(event.isFree()).isEqualTo(isFree);
     }
 
-    @Test
-    void testOfflineTrue(){
+    @ParameterizedTest(name = "{index} => basePrice={0}, maxPrice={1}, isFree={2}")
+    @MethodSource("paramsForTestFree")
+    public void various_tests_isFree_Safety(int basePrice, int maxPrice, boolean isFree){
+        System.out.println(basePrice);
         // Given
         Event event = Event.builder()
-                .location("강남역 네이버 D2 스타텁 팩토리")
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
-
-        // When
+        //When
         event.update();
 
         // Then
-        assertThat(event.isOffline()).isTrue();
+        assertThat(event.isFree()).isEqualTo(isFree);
     }
 
-    @Test
-    void testOfflineFalse(){
+    private static Object[] paramsForTestFree(){
+        return new Object[]{
+                new Object[]{0,0, true},
+                new Object[]{100,0, false},
+                new Object[]{0,1000, false},
+        };
+    }
+
+    @ParameterizedTest(name = "{index} => location={0}, offline={1}")
+    @MethodSource("paramsForTestOffline")
+    void testOffline(String location, boolean offline){
         // Given
         Event event = Event.builder()
+                .location(location)
                 .build();
 
         // When
         event.update();
 
         // Then
-        assertThat(event.isOffline()).isFalse();
+        assertThat(event.isOffline()).isEqualTo(offline);
+    }
+
+    private static Object[] paramsForTestOffline(){
+        return new Object[]{
+                new Object[]{"강남역 네이버 D2 스타텁 팩토리", true},
+                new Object[]{null, false},
+        };
     }
 
 }
