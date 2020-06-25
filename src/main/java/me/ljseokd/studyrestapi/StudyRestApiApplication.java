4f23c8ca982f1a6db5920012b16d1cd2598b0dca
JoongSeokD <ljseokd@gmail.com>
2020-06-25 16:20:02 +0900

    스프링 시큐리티 기본 설정

--- src/main/java/me/ljseokd/studyrestapi/StudyRestApiApplication.java
+++ src/main/java/me/ljseokd/studyrestapi/StudyRestApiApplication.java
@@ -12,9 +12,4 @@ public class StudyRestApiApplication {
         SpringApplication.run(StudyRestApiApplication.class, args);
     }
 
-    @Bean
-    ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
-
 }
